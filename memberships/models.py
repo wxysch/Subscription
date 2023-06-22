5ed7c1ec559cd123c56a5a9410d76410f9c6a2c0
alinu1 <uwu1343@gmail.com>
2023-06-22 11:58:03 +0600

    add some new models and settings

--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+
+import stripe
 
 # Create your models here.
+MEMBERSHIP_CHOICES = (
+    ('Enterprise', 'ent'),
+    ('Professional', 'pro'),
+    ('Free','free')
+)
+
+class Memberships(models.Model):
+    slug = models.SlugField()
+    membership_type = models.CharField(max_length=30,
+                                       choices=MEMBERSHIP_CHOICES,
+                                       default='Free')
+    price=models.IntegerField(default=15)
+    stripe_plan_id=models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.membership_type
+    
+class UserMembership(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True,null=True)
+    stripe_customer_id=models.CharField(max_length=40)
+    membership = models.ForeignKey(Memberships,on_delete=models.CASCADE,blank=True,null=True)
+
+    def __str__(self):
+        return self.user.username
+    
+def post_save_usermembership_create(sender,instance, created, *args, **kwargs):
+    if created:
+        UserMembership.objects.get_or_create(user=instance)
+    user_membership,created=UserMembership.objects.get_or_create(user=instance)
+    if user_membership.stripe_customer_id is None or user_membership.stripe_customer_id== '':
+        new_customer_id=stripe.Customer.create(email=instance.email)
+        user_membership.stripe_customer_id = new_customer_id["id"]
+        user_membership.save()
+
+post_save.connect(post_save_usermembership_create, sender=settings.AUTH_USER_MODEL)
+
+class Subcription(models.Model):
+    user_membership = models.ForeignKey(UserMembership,on_delete=models.CASCADE,blank=True,null=True)
+    stripe_subcription = models.CharField(max_length=40)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user_membership.user.username
--- videoservice/settings.py
+++ videoservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'videoservice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,9 +117,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    STRIPE_PUBLISHABLE_KEY = ''
+    STRIPE_SECRET_KEY = ''
+
+else:
+    STRIPE_PUBLISHABLE_KEY = ''
+    STRIPE_SECRET_KEY = '' 
