53bca50e082e0b0f20fcdb809c26dc8af9519c7d
alinu1 <uwu1343@gmail.com>
2023-06-22 15:31:20 +0600

    new html

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from memberships.models import Memberships
 
+from django.urls import reverse
+
 # Create your models here.
 class Courses(models.Model):
     slug = models.SlugField()
@@ -11,6 +13,9 @@ class Courses(models.Model):
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        return reverse('courses:detail',kwargs={'slug':self.slug})
+
 class Lesson(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=120)
